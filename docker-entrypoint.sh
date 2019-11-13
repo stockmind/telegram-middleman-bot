7f640d83f95f0962f9072bb45010c5e449fb4efb
stockmind <slysimo@gmail.com>
2019-11-13 17:52:09 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,5 @@
 set -e
 
 cd /go/src/app
-
-echo "Set webhook..."
-curl -F "url=https://$HOST/api/updates" -F "certificate=/cert.pem" "https://api.telegram.org/bot$BOT_TOKEN/setWebhook"
 echo "Launch app..."
 app --token $BOT_TOKEN --port $PORT --useHttps $HTTPS --rateLimit $RATELIMIT --mode $MODE $@
